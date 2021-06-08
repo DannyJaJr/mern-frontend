8497019f974993f01bbe94fa4745c55a7f0782ad
Daniel Lafontant <ldanieljajr@gmail.com>
2021-06-08 12:49:32 -0400

    handleSubmit at Signup.js

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -5,42 +5,92 @@ import { Redirect } from 'react-router-dom';
 const REACT_APP_SERVER_URL = process.env.REACT_APP_SERVER_URL;
 
 const Signup = () => {
-  //create react hooks
-  const [name, setName ]= useState("")
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('')
-const [confirmPassword, setConfirmPassword] = useState('')
-const [redirect, setRedireect] = useState(false)
+    //create react hooks
+    const [name, setName] = useState("")
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('')
+    const [confirmPassword, setConfirmPassword] = useState('')
+    const [redirect, setRedirect] = useState(false)
 
 
-const handleName = (e) => {
-    setName(e.target.value)
-}
-
-const handleEmail = (e) => {
-    setEmail(e.target.value)
-}
-
-const handlePassword = (e) => {
-    setPassword(e.target.value)
-}
-
-const handleConfirmPassword = (e) => {
-    setConfirmPassword(e.target.value)
-}
-
-
-
-    const handleSubmit = (e) => {
-
+    const handleName = (e) => {
+        setName(e.target.value)
     }
-    
+
+    const handleEmail = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const handlePassword = (e) => {
+        setPassword(e.target.value)
+    }
+
+    const handleConfirmPassword = (e) => {
+        setConfirmPassword(e.target.value)
+    }
+
+
+
+    // const handleSubmit = (e) => {
+    //    e.preventDefault();
+    //    //check to make sure paswords match
+    //    if (password === confirmPassword && password.length >= 8) {
+    //        const payload = { name, email, password };
+    //        //now make the post call
+    //        let url =`${REACT_APP_SERVER_URL}/api/users/signup`;
+    //        axios.post(url, payload)
+    //        .then(response => {
+    //            console.log(response.data)
+    //         setRedirect(true)
+
+    //        })
+    //        .catch(error => {
+    //            console.log(error)
+    //        })
+
+    //    }else {
+    //        alert('Password needs to be at least 8 characters or more. Please try again')
+    //    }
+
+    // }
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        // check to make sure passwords match
+        if (password === confirmPassword && password.length >= 8) {
+            const payload = { name, email, password };
+            let url = `${REACT_APP_SERVER_URL}/api/users/signup`;
+            try {
+                let response = await axios.post(url, payload);
+                let { data } = response;
+                console.log(data);
+                setRedirect(true);
+            } catch (error) {
+                alert('Error occurred, please try again...');
+            }
+        } else {
+            if (!password === confirmPassword) {
+                alert('Password and Confirm Password need to match. Please try again...');
+            } else {
+                alert('Password needs to be at least 8 characters or more. Please try again...');
+            }
+        }
+    }
+
+    if (redirect) return <Redirect to='/login' />
+
+
+
+
+
+
     return (
         <div className="row mt-4">
             <div className="col-md-7 offset-md-3">
                 <div className="card card-body">
                     <h2 className="py-2">Signup</h2>
-                    <Form  onSubmit={handleSubmit}>
+                    <Form onSubmit={handleSubmit}>
+
                         <div className="form-group">
                             <lable htmlFor="name">Name</lable>
                             <imput type="text" name="name" value={name} onChange={handleName} className="form-control" />
@@ -48,26 +98,26 @@ const handleConfirmPassword = (e) => {
 
 
                         <div className="form-group">
-                        <lable htmlFor="email">Email</lable>
+                            <lable htmlFor="email">Email</lable>
                             <imput type="text" name="email" value={email} onChange={handleEmail} className="form-control" />
                         </div>
 
                         <div className="form-group">
-                        <lable htmlFor="name">Password</lable>
+                            <lable htmlFor="name">Password</lable>
                             <imput type="password" name="password" value={password} onChange={handlePassword} className="form-control" />
                         </div>
 
                         <div className="form-group">
-                        <lable htmlFor="confirmPassword">Confirm Password</lable>
+                            <lable htmlFor="confirmPassword">Confirm Password</lable>
                             <imput type="password" name="confirmPassword" value={confirmPassword} onChange={handleConfirmPassword} className="form-control" />
                         </div>
 
-    
+
                         <button type="submit" className="btn btn-primary float-right">Submit</button>
 
 
                     </Form>
-                    
+
                 </div>
             </div>
         </div>
